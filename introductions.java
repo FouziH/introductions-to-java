06db4b034619eaec278a3416332b573b479cae55
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-12-22 11:41:34 -0600

    camelCase

--- introductions.java
+++ introductions.java
@@ -4,7 +4,7 @@ public class introductions {
     public static void main(String args[]){
         Console console = System.console();
         //Welcome to the Introductions program! Your code goes below here
-
+        String firstName = "Hamza";  //thisIsAnExampleOfCamelCasing
         console.printf("Hello, my name is Craig\n");
         console.printf("Craig is learning how to Java");
     }
