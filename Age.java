65f58eaf58cc62234c5d4917dc2021d4830bf797
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-12-22 16:37:17 -0600

    ...

--- Age.java
+++ Age.java
@@ -2,6 +2,9 @@ import java.io.Console;
 public class Age {
     public static void main(String[] args) {
          Console console = System.console();
+         //declare age using int variable
+
+         int age = 12;
 
     }
     
