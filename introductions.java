9be9f994671717e457bb134566babbbe1ffda5c7
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-12-22 12:04:10 -0600

    ...

--- introductions.java
+++ introductions.java
@@ -4,7 +4,7 @@ public class introductions {
     public static void main(String args[]){
         Console console = System.console();
         //Welcome to the Introductions program! Your code goes below here
-        String firstName = "Hassan";  //thisIsAnExampleOfCamelCasing
+        String firstName = console.readLine("What is your name? ");
         console.printf("Hello, my name is %s\n", firstName);
         console.printf("%s is learning how to write Java", firstName);
     }
