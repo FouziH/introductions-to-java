a5a2c8cfc97723aeddba4b350289a24d60392edf
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-12-22 16:54:09 -0600

    ...

--- Age.java
+++ Age.java
@@ -4,12 +4,12 @@ public class Age {
          Console console = System.console();
          //declare age using int variable
 
-         int age = 12;
-         if(age < 13) {
-             //insert our exit code
-             console.printf("You must be at least 13 years of age to use this program");
-             System.exit(0);
-         }
+        //  int age = 12;
+        //  if(age < 13) {
+        //      //insert our exit code
+        //      console.printf("You must be at least 13 years of age to use this program");
+        //      System.exit(0);
+        //  }
 
          int numberOfPeople = 3;
 
