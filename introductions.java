07c8706c6097cf67585d8fd56625d37970d2cb61
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-12-22 11:34:23 -0600

    ....intro to java

--- introductions.java
+++ introductions.java
@@ -4,6 +4,8 @@ public class introductions {
     public static void main(String args[]){
         Console console = System.console();
         //Welcome to the Introductions program! Your code goes below here
+
         console.printf("Hello, my name is Craig");
+        console.printf("Craig is learning how to Java");
     }
 }
