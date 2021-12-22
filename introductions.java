822373f5bae713eae01faf01806e1c77a06c8df5
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-12-22 11:38:57 -0600

    intro to space

--- introductions.java
+++ introductions.java
@@ -5,7 +5,7 @@ public class introductions {
         Console console = System.console();
         //Welcome to the Introductions program! Your code goes below here
 
-        console.printf("Hello, my name is Craig");
+        console.printf("Hello, my name is Craig\n");
         console.printf("Craig is learning how to Java");
     }
 }
