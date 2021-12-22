cf3263001a2c327a03a6644465b30c1a376f2098
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-12-22 11:18:46 -0600

    ...

---
+++ introductions.java
@@ -0,0 +1,9 @@
+import java.io.Console;
+
+public class introductions {
+    public static void main(String args[]){
+        Console console = System.console();
+        //Welcome to the Introductions program! Your code goes below here
+        console.printf("Hello, my name is Craig");
+    }
+}
